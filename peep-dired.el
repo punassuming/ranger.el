efcdfbe0d504a79c90f176fde01b44cb9c0a237c
asok <adam.sokolnicki@gmail.com>
2014-04-24 23:18:31 +0200

    Add elisp library headers.

--- peep-dired.el
+++ peep-dired.el
@@ -1,3 +1,29 @@
+;;; peep-dired.el --- A minor mode for dired buffers which allows to peep files in other window  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  Adam Sokolnicki
+
+;; Author: Adam Sokolnicki <adam.sokolnicki@gmail.com>
+;; Keywords: files, convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
 (defvar peep-dired-mode-map
   (let ((map (make-sparse-keymap)))
     (define-key map (kbd "<down>")      'peep-dired-next-file)
@@ -74,3 +100,5 @@
     (peep-dired-disable)))
 
 (provide 'peep-dired)
+
+;;; peep-dired.el ends here
