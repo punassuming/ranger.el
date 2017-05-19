5defa6215f5648f68a804aa9109c5b9fcc793490
ralesi <scio62@gmail.com>
2017-05-18 21:33:53 -0700

    Fix file numbering in footer.

--- ranger.el
+++ ranger.el
@@ -1663,8 +1663,8 @@ R   : ranger . el location
            (file-date (format-time-string "%Y-%m-%d %H:%m"
                                           (nth 5 fattr)))
            (file-perm (nth 8 fattr))
-           (cur-pos (line-number-at-pos (point)))
-           (final-pos (- (line-number-at-pos (point-max)) 1))
+           (cur-pos (- (line-number-at-pos (point)) 1))
+           (final-pos (- (line-number-at-pos (point-max)) 2))
            (position (format "%3d/%-3d"
                              cur-pos
                              final-pos))
